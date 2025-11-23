51e62538ed01b8ac7ab360378373bc246d14c556
Victor Soares <victorsoaresho@gmail.com>
2025-11-23 17:32:05 -0300

    TRABALHO 2

--- front-end/src/ui/FooterBar.jsx
+++ front-end/src/ui/FooterBar.jsx
@@ -24,7 +24,7 @@ export default function FooterBar() {
           }
         }}
       >
-        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:professor@faustocintra.com.br">Prof. Fausto Cintra</a>
+        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:victorsoaresho@gmail.com">Victor Soares</a>
       </Typography>
     </Toolbar>
   )
